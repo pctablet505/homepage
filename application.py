32ca92f3cd23d3263a4cb43d1d46f4f85a6e0fd3
pctablet505 <rahul@DESKTOP-KE3GL8C.localdomain>
2021-05-24 14:40:49 +0530

    added projects

--- application.py
+++ application.py
@@ -1,9 +1,7 @@
 import os
 from flask import Flask, render_template, redirect
 
-
 app = Flask(__name__)
-
 
 files = os.listdir(path='./templates')
 routes = []
@@ -17,97 +15,38 @@ for x in files:
 def index():
     path = ('./static/me')
     photos = os.listdir(path=path)
-    print('\n'*5)
+    print('\n' * 5)
     print(photos)
-    print('\n'*5)
+    print('\n' * 5)
     return render_template('home.html', routes=routes, os=os, photos=photos, path=path)
 
 
 @app.route('/contact')
 def contact():
-
     contacts = {
         'Mail': ('pctablet505@gmail.com', 'mailto:pctablet505@gmail.com'),
         'Phone': ('+91 8709253658', 'tel:+91 8709253658'),
         'LinkedIn': ('@pctablet505', 'https://www.linkedin.com/in/pctablet505'),
         'Hackerrank': ('@pctablet505', 'https://www.hackerrank.com/pctablet505'),
         'GitHub': ('@pctablet505', 'https://www.github.com/pctablet505'),
-        'Instagram': ('pctablet505', 'https://www.instagram.com/pctablet505')
+        'Instagram': ('pctablet505', 'https://www.instagram.com/pctablet505'),
     }
     return render_template('contact.html', routes=routes, contacts=contacts)
 
 
 @app.route('/certifications')
 def certifications():
-
-    certificates = [
-        {'credential': 'https://www.hackerrank.com/certificates/58d066424a1e',
-         'src': 'static/certificates/C Advanced.png',
-         'title': 'C Advanced'},
-        {'credential': 'https://www.hackerrank.com/certificates/0c61249bbe1d',
-            'src': 'static/certificates/C Basic.png',
-            'title': 'C Basic'},
-        {'credential': 'https://www.hackerrank.com/certificates/337182b9dc84',
-            'src': 'static/certificates/C Intermediate.png',
-            'title': 'C Intermediate'},
-        {'credential': 'https://cs50.harvard.edu/certificates/2b1773c1-473d-4a7b-89ae-460d23154126',
-            'src': 'static/certificates/CS50 AI.png',
-            'title': 'CS50 AI'},
-        {'credential': 'https://www.hackerrank.com/certificates/92d056e955d2',
-            'src': 'static/certificates/CSS.png',
-            'title': 'CSS'},
-        {'credential': 'https://www.hackerrank.com/certificates/253bacd0f0c0',
-            'src': 'static/certificates/Java.png',
-            'title': 'Java'},
-        {'credential': 'https://www.kaggle.com/learn/certification/pctablet505/intermediate-machine-learning',
-            'src': 'static/certificates/Kaggle - Intermediate Machine Learning.png',
-            'title': 'Kaggle Intermediate Machine Learning'},
-        {'credential': 'https://www.kaggle.com/learn/certification/pctablet505/intro-to-deep-learning',
-            'src': 'static/certificates/Kaggle - Intro to Deep Learning.png',
-            'title': 'Kaggle Intro to Deep Learning'},
-        {'credential': 'https://www.kaggle.com/learn/certification/pctablet505/intro-to-machine-learning',
-            'src': 'static/certificates/Kaggle - Intro to Machine Learning.png',
-            'title': 'Kaggle Intro to Machine Learning'},
-        {'credential': 'https://www.hackerrank.com/certificates/fc7b9af8aac8',
-            'src': 'static/certificates/problem Solving Advanced.png',
-            'title': 'Problem Solving Advanced'},
-        {'credential': 'https://www.hackerrank.com/certificates/e3c6568f71c6',
-            'src': 'static/certificates/Problem Solving Basic.png',
-            'title': 'Problem Solving Basic'},
-        {'credential': 'https://www.hackerrank.com/certificates/f5bcd6aa9355',
-            'src': 'static/certificates/Problem Solving Intermediate.png',
-            'title': 'Problem Solving Intermediate'},
-        {'credential': 'https://www.hackerrank.com/certificates/6e9c190da189',
-            'src': 'static/certificates/Python Advanced.png',
-            'title': 'Python Advanced'},
-        {'credential': 'https://www.hackerrank.com/certificates/93d10484c124',
-            'src': 'static/certificates/Python Basic.png',
-            'title': 'Python Basic'},
-        {'credential': 'https://www.hackerrank.com/certificates/9ecfb2a355a2',
-            'src': 'static/certificates/Python Intermediate.png',
-            'title': 'Python Intermediate'},
-        {'credential': 'https://www.hackerrank.com/certificates/2508bf1f2b12',
-            'src': 'static/certificates/SQL Basic.png',
-            'title': 'SQL Basic'},
-        {'credential': 'http://ude.my/UC-d7c52c49-464f-4b63-a9b4-2da5d4c53ff4/',
-            'src': 'static/certificates/Machine Learning.jpg',
-            'title': 'Udemy Complete 2020 Data Science & Machine Learning Bootcamp'},
-        {'credential': 'https://www.udemy.com/certificate/UC-37c2faaf-17c4-4089-ab2d-18190a8aa61f/',
-            'src': 'static/certificates/DSA.jpg',
-            'title': 'Udemy Data Structures and Algorithms'},
-        {'credential': 'https://www.kaggle.com/learn/certification/pctablet505/intermediate-machine-learning',
-            'src': 'static/certificates/Python.jpg',
-            'title': 'Udemy Python'},
-        {'credential': 'https://www.kaggle.com/learn/certification/pctablet505/python',
-            'src': 'static/certificates/Kaggle - Python.png',
-            'title': 'kaggle Python'},
-        {'credential': 'https://www.hackerrank.com/certificates/74470a9fe364',
-            'src': 'static/certificates/SQL Intermediate.png',
-            'title': 'SQL Intermediate'},
-    ]
-    certificates.sort(key=lambda x: x['title'])
+    from certificates import certificates
+    certificates.sort(key=lambda x: x.title)
 
     return render_template('certifications.html', routes=routes, certificates=certificates)
+
+
+@app.route('/projects')
+def projects():
+    from projects_list import projects_list
+
+    return render_template('projects.html', routes=routes, projects=projects_list)
 
 
 @app.route('/home')
@@ -118,4 +57,3 @@ def home():
 @app.route('/<name>')
 def route(name):
     return render_template(f'{name}.html', routes=routes)
-
---
+++ projects_list.py
@@ -0,0 +1,103 @@
+from collections import namedtuple
+
+Project = namedtuple(
+    'Project', ['title', 'description', 'demonstration', 'code'])
+projects_list = [
+    Project('PacMan',
+            '''AI based intelligent agent to control pacman in different environments''',
+            '''https://inst.eecs.berkeley.edu/~cs188/sp21/project1/''',
+            'https://github.com/pctablet505/CS188-AI/tree/main/proj1-search-python3'
+            ),
+    Project(
+        'Automatic Review Analyzer',
+        '''Sentiment Analysis for customer reviews''',
+        '',
+        'https://github.com/pctablet505/MIT-6.86x-2021-feb/tree/main/Project1/sentiment_analysis'
+
+    ),
+    Project(
+        'MNIST Digit Recognition',
+        '''Classification of digits using support vector machine and gradient descent.''',
+        '',
+        'https://github.com/pctablet505/MIT-6.86x-2021-feb/tree/main/Project2/mnist/part1'
+
+    ),
+    Project(
+        'Overlapping Digit Recognition',
+        '''Classification of overlapping digits using Convolutional Neural Networks.''',
+        '',
+        'https://github.com/pctablet505/MIT-6.86x-2021-feb/tree/main/Project2/mnist/part2-twodigit'
+
+    ),
+    Project(
+        'Collaborative Filtering via Gaussian Mixtures',
+        'Recommender system for Netflix using Gaussian Mixtures models and EM algorithm.',
+        '',
+        'https://github.com/pctablet505/MIT-6.86x-2021-feb/tree/main/project4/resources_netflix/netflix'
+    ),
+    Project(
+        'Text game using Reinforcement Learning',
+        '''It is a game in which a robot is given task in a room and he learns to play it.
+            Implementations using Q learning, Q learning with approximations, using neural networks.''',
+        '',
+        'https://github.com/pctablet505/MIT-6.86x-2021-feb/tree/main/project5/rl'
+    ),
+    Project(
+        "Maze Solver",
+        '''Demonstration of complexity of different search algorithms ''',
+        '',
+        'https://github.com/pctablet505/CS50-AI'
+    ),
+    Project(
+        'Degrees',
+        '''Calculation of minimum degree of separation between actors in holywood.
+            Separation is in terms of working together.''',
+        'https://cs50.harvard.edu/ai/2020/projects/0/degrees/',
+        'https://github.com/pctablet505/CS50-AI/tree/main/degrees'
+
+    ),
+    Project(
+        'Tic-Tac-Toe',
+        'MiniMax based undefeatable Intelligent to play against human.',
+        'https://www.youtube.com/watch?v=tp9DjqV_KoU',
+        'https://github.com/pctablet505/CS50-AI/tree/main/tictactoe'
+
+    ),
+    Project(
+        'Minesweeper',
+        '''Knowledge based Intelligent agent to play Minesweeper which reasons using preposotional logic.''',
+        'https://cs50.harvard.edu/ai/2020/projects/1/minesweeper/',
+        'https://github.com/pctablet505/CS50-AI/tree/main/minesweeper'
+    ),
+    Project(
+        'CSP solver for Crossword Puzzle',
+        '''Solves Crossword puzzles by modelling it into Constraint satisfaction problem and then using backtracking search with different heuristics''',
+        'https://cs50.harvard.edu/ai/2020/projects/3/crossword/',
+        'https://github.com/pctablet505/CS50-AI/tree/main/crossword'
+    ),
+    Project(
+        'Pabilistic models for reasoning under uncertainity',
+        '''To demonstrate page ranking and gene inheritance''',
+        'https://cs50.harvard.edu/ai/2020/projects/2/',
+        'https://github.com/pctablet505/CS50-AI/tree/main/probabilistic%20models'
+    ),
+    Project(
+        'Shopping',
+        'KNN model to predict whether customer will make the purchase or not.',
+        'https://cs50.harvard.edu/ai/2020/projects/4/shopping/',
+        'https://github.com/pctablet505/CS50-AI/tree/main/shopping'
+    ),
+    Project(
+        'NIM',
+        '''Playing NIM against human. Trained using reinforcement learning.''',
+        'https://cs50.harvard.edu/ai/2020/projects/4/nim/',
+        'https://github.com/pctablet505/CS50-AI/tree/main/nim'
+
+    ),
+    Project(
+        'Traffic Sign classification',
+        '''CNN model for 43 class classification of traffic sign boards''',
+        'https://cs50.harvard.edu/ai/2020/projects/5/traffic/',
+        'https://github.com/pctablet505/CS50-AI/tree/main/traffic'
+
+    )]
