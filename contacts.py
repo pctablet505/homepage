696f1367d74d0077300f7ba37d2dd74a917ab62e
RAHUL KUMAR <pctablet505@gmail.com>
2021-06-04 14:17:38 +0530

    updated about in resume

--- contacts.py
+++ contacts.py
@@ -1,4 +1,5 @@
 contacts = {
+    'Website': ('https://pctablet505.herokuapp.com', 'https://pctablet505.herokuapp.com/about'),
     'Mail': ('pctablet505@gmail.com', 'mailto:pctablet505@gmail.com'),
     'Phone': ('+91 8709253658', 'tel:+91 8709253658'),
     'LinkedIn': ('@pctablet505', 'https://www.linkedin.com/in/pctablet505'),
