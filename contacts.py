7e3f8769821bebf5407e8eaf229d5fdb931e7122
pctablet505 <rahul@DESKTOP-5TN55RE.localdomain>
2021-07-21 03:47:01 +0530

    changes

--- contacts.py
+++ contacts.py
@@ -1,7 +1,7 @@
 contacts = {
     'Website': ('https://pctablet505.herokuapp.com', 'https://pctablet505.herokuapp.com/about'),
     'Mail': ('pctablet505@gmail.com', 'mailto:pctablet505@gmail.com'),
-    'Phone': ('+91 8709253658', 'tel:+91 8709253658'),
+    'Phone': ('+91 8709253658', 'tel:+91-8709253658'),
     'LinkedIn': ('@pctablet505', 'https://www.linkedin.com/in/pctablet505'),
     'Hackerrank': ('@pctablet505', 'https://www.hackerrank.com/pctablet505'),
     'GitHub': ('@pctablet505', 'https://www.github.com/pctablet505'),
