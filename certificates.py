9a9bca5ce983c03e75fd7ff525797b974d4a5feb
RAHUL KUMAR <55033230+pctablet505@users.noreply.github.com>
2021-09-03 14:29:31 +0530

    Update certificates.py

--- certificates.py
+++ certificates.py
@@ -3,6 +3,8 @@ from collections import namedtuple
 Certificate = namedtuple('Certificate', ['credential', 'src', 'title'])
 
 certificates = [
+    Certificate(credential='https://courses.edx.org/certificates/45213294fe2649c5a5954e4c42a70f13',
+                src='static/certificates/MIT 6.431x.png', title='MIT Probability - The Science of Uncertainity'),
     Certificate(credential='https://courses.edx.org/certificates/8cbde197f6874661ab8794d753f9fd7b',
                 src='static/certificates/MIT 6.86x.jpg', title='MIT Machine Learning with Python-From Linear Models to Deep Learning'),
     Certificate(credential='https://cs50.harvard.edu/certificates/2b1773c1-473d-4a7b-89ae-460d23154126',
